612cd3189d805a2ad2299ae8bb622ef1a92dacf2
Manojkumar <manojprfp@gmail.com>
2025-08-13 16:17:05 +0530

    skills added

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -11,11 +11,10 @@ const Landing = ({setSelectedPage}) =>{
             <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
             {isAboveMediumScreens ? 
             (
-                <div className="relative z-0 ml-20 before:absolute before:top-20 before:-left-20
-                before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full
-                before:border-2 before: border-blue before:z-[-1]">
-                    <img alt="profile" 
-                    src="assets/manoj" 
+                <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20
+                before:w-full before:h-full before:border-2 before: border-blue before:z-[-1]">
+                    <img alt="skills" 
+                    src="assets/skills-image.png" 
                     className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full
                     max-w-[400px] md:max-w-[600px]"/>
                 </div>
---
+++ src/scenes/MySkills.jsx
@@ -0,0 +1,121 @@
+import { motion } from "framer-motion";
+import LineGradient from "../components/LineGradient";
+import useMediaQuery from "../hooks/useMediaQuery";
+
+const MySkills = () => {
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    return(
+        <section id="skills" className="pt-10 pb-24">
+            {/* Header and Image Section */}
+            <div className="md:flex md:justify-between md:gap-16 mt-32">
+                <motion.div
+                className="md:w-1/3"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{once:true, amount:0.5}}
+                transition={{delay:0.4, duration:0.5}}
+                variants={{
+                hidden: { opacity:0, x:-50},
+                visible: { opacity: 1,x:0}
+                }}
+                >
+                    <p className="font-playfair font-semibold text-4xl mb-5">
+                        My <span className="text-red">SKILLS</span>
+                    </p>
+                    <LineGradient width="w-1/3" />
+                    <p className="mt-10 mb-7">
+                        Here are my skills
+                    </p>
+                </motion.div>
+                <div className="mt-16 md:mt-0">
+                    {isAboveMediumScreens? (
+                        <div className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+                before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full
+                before:border-2 before: border-blue before:z-[-1]">
+                    <img alt="skills" 
+                    src="assets/skills-image.png" 
+                    className="z-10"/>
+                </div>
+                    ):(
+                        <img alt="skills" className="z-10" src="assets/skills-image.png"/>
+                    )}
+                </div>
+            </div>
+            {/* Skills */}
+            <div className="md:flex md:justify-between mt-16 gap-32">
+                {/* Experience */}
+                <motion.div
+                className="md:w-1/3 mt-10"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{once:true, amount:0.5}}
+                transition={{delay:0.4, duration:0.5}}
+                variants={{
+                hidden: { opacity:0, x:50},
+                visible: { opacity: 1,x:0}
+                }}
+                >
+                    <div className="relative h-32">
+                        <div className="z-10">
+                            <p className="font-playfair font-semibold text-5xl">01</p>
+                            <p className="font-playfair font-semibold text-3xl">Experience</p>
+                        </div>
+                        <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]"/>
+                        <p className="mt-5">
+                            Hello
+                        </p>
+                    </div>
+                </motion.div>
+                {/* INNOVATIVE */}
+                <motion.div
+                className="md:w-1/3 mt-10"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{once:true, amount:0.5}}
+                transition={{delay:0.2, duration:0.5}}
+                variants={{
+                hidden: { opacity:0, x:50},
+                visible: { opacity: 1,x:0}
+                }}
+                >
+                    <div className="relative h-32">
+                        <div className="z-10">
+                            <p className="font-playfair font-semibold text-5xl">02</p>
+                            <p className="font-playfair font-semibold text-3xl">Innovative</p>
+                        </div>
+                        <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]"/>
+                        <p className="mt-5">
+                            Innovative text
+                        </p>
+                    </div>
+                </motion.div>
+                {/* Imaginative */}
+                <motion.div
+                className="md:w-1/3 mt-10"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{once:true, amount:0.5}}
+                transition={{delay:0.4, duration:0.5}}
+                variants={{
+                hidden: { opacity:0, x:50},
+                visible: { opacity: 1,x:0}
+                }}
+                >
+                    <div className="relative h-32">
+                        <div className="z-10">
+                            <p className="font-playfair font-semibold text-5xl">03</p>
+                            <p className="font-playfair font-semibold text-3xl">Imaginative</p>
+                        </div>
+                        <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]"/>
+                        <p className="mt-5">
+                            Imaginative text
+                        </p>
+                    </div>
+                </motion.div>
+                
+            </div>
+        </section>
+    )
+}
+
+export default MySkills
